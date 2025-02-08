5a8727981a3aa71f13078e1db0afc1a2e051a8b1
JAICHANGPARK <aristojeff@gmail.com>
2025-02-08 09:07:46 +0900

    Update ep2151_main.dart

--- lib/ep2151_e_learning_app/ep2151_main.dart
+++ lib/ep2151_e_learning_app/ep2151_main.dart
@@ -10,7 +10,7 @@ class ELearningApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ELearningApp(),
+      home: ELearningHomePage(),
     );
   }
 }
