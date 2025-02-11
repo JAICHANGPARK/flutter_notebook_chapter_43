35f44481e6c8768b0e6109360a58b7c38b9804ca
JAICHANGPARK <aristojeff@gmail.com>
2025-02-11 18:49:29 +0900

    Update paywall_screen.dart

--- lib/ep2154_paywall_screen/views/paywall_screen.dart
+++ lib/ep2154_paywall_screen/views/paywall_screen.dart
@@ -44,7 +44,7 @@ class _PaywallScreenState extends State<PaywallScreen> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 32),
+                    padding: const EdgeInsets.symmetric(vertical: 48),
                     child: Center(
                       child: Text(
                         "Subscribe to reach\nyour financial goals",
@@ -57,7 +57,7 @@ class _PaywallScreenState extends State<PaywallScreen> {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(
-                      left: 24,
+                      left: 32,
                     ),
                     child: Column(
                       spacing: 16,
