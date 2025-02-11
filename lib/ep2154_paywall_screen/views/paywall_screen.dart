c89286f88d801f19f243598804c1315c9ccf2e83
JAICHANGPARK <aristojeff@gmail.com>
2025-02-11 18:42:40 +0900

    Update paywall_screen.dart

---
+++ lib/ep2154_paywall_screen/views/paywall_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PaywallScreen extends StatefulWidget {
+  const PaywallScreen({super.key});
+
+  @override
+  State<PaywallScreen> createState() => _PaywallScreenState();
+}
+
+class _PaywallScreenState extends State<PaywallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
