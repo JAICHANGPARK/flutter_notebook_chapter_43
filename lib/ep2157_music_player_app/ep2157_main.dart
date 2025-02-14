f83c05dccae930cd9dddbc3438526f86dff3e8c2
JAICHANGPARK <aristojeff@gmail.com>
2025-02-14 17:40:32 +0900

    Update ep2157_main.dart

--- lib/ep2157_music_player_app/ep2157_main.dart
+++ lib/ep2157_music_player_app/ep2157_main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void  main() {
-  
+
+}
+
+class MusicPlayerApp extends StatelessWidget {
+  const MusicPlayerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
 }
