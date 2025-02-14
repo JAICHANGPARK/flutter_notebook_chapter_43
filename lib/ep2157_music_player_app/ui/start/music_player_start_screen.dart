9c2df15cc495ed103ea3e6a2358b82b599b78e9b
JAICHANGPARK <aristojeff@gmail.com>
2025-02-14 17:46:10 +0900

    Update music_player_start_screen.dart

---
+++ lib/ep2157_music_player_app/ui/start/music_player_start_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPlayerStartScreen extends StatefulWidget {
+  const MusicPlayerStartScreen({super.key});
+
+  @override
+  State<MusicPlayerStartScreen> createState() => _MusicPlayerStartScreenState();
+}
+
+class _MusicPlayerStartScreenState extends State<MusicPlayerStartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
