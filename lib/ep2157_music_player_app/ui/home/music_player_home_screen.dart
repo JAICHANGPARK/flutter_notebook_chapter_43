7b59cd8cec895de1496a6983a298f689dcda8711
JAICHANGPARK <aristojeff@gmail.com>
2025-02-16 10:14:29 +0900

    Update music_player_home_screen.dart

--- lib/ep2157_music_player_app/ui/home/music_player_home_screen.dart
+++ lib/ep2157_music_player_app/ui/home/music_player_home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MusicPlayerHomeScreen extends StatefulWidget {
   const MusicPlayerHomeScreen({super.key});
 
@@ -12,7 +11,35 @@ class _MusicPlayerHomeScreenState extends State<MusicPlayerHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 42,
+                  width: 42,
+                  decoration: BoxDecoration(),
+                ),
+                Spacer(),
+                Container(
+                  height: 42,
+                  width: 42,
+                  decoration: BoxDecoration(),
+                ),
+                Container(
+                  height: 42,
+                  width: 42,
+                  decoration: BoxDecoration(),
+                ),
+
+
+
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
