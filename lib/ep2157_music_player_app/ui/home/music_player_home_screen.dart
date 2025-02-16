4c080e82a771f03590ead68bf5aecb5cea2babf6
JAICHANGPARK <aristojeff@gmail.com>
2025-02-16 10:15:32 +0900

    Update music_player_home_screen.dart

--- lib/ep2157_music_player_app/ui/home/music_player_home_screen.dart
+++ lib/ep2157_music_player_app/ui/home/music_player_home_screen.dart
@@ -14,33 +14,36 @@ class _MusicPlayerHomeScreenState extends State<MusicPlayerHomeScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              spacing: 12,
-              children: [
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(),
-                ),
-                Spacer(),
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(),
-                ),
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 0.5,
-                      ),
-                      borderRadius: BorderRadius.circular(
-                        4,
-                      )),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                spacing: 12,
+                children: [
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(),
+                  ),
+                  Spacer(),
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(),
+                  ),
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 0.5,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          4,
+                        )),
+                  ),
+                ],
+              ),
             )
           ],
         ),
