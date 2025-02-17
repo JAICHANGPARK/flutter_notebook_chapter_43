bc4a67bb88c71710c2b108eff74cc1994ac8a0f5
JAICHANGPARK <aristojeff@gmail.com>
2025-02-17 17:32:57 +0900

    Create music_player_idm_page.dart

---
+++ lib/ep2157_music_player_app/ui/home/widgets/music_player_idm_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerIdmPage extends StatelessWidget {
+  const MusicPlayerIdmPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
