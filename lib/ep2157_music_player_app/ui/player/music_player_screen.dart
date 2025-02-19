352ca08d1200f79cfda5ccb6f73876ac21431f58
JAICHANGPARK <aristojeff@gmail.com>
2025-02-19 19:02:50 +0900

    Update music_player_screen.dart

--- lib/ep2157_music_player_app/ui/player/music_player_screen.dart
+++ lib/ep2157_music_player_app/ui/player/music_player_screen.dart
@@ -97,6 +97,7 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                           "Dream Youth",
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
+                            fontSize: 18,
                           ),
                         ),
                         Container(
@@ -115,7 +116,14 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                               color: Colors.white,
                             ),
                           ),
-                        )
+                        ),
+                        Expanded(
+                            child: SingleChildScrollView(
+                          child: Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                            style: TextStyle(height: 1.2),
+                          ),
+                        ))
                       ],
                     ),
                   ),
