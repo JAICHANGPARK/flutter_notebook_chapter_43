5fb120c39692bf6550d3a169377324e0e4bfec29
JAICHANGPARK <aristojeff@gmail.com>
2025-02-20 19:29:58 +0900

    Update ep2163_main.dart

--- lib/ep2163_newsfeed_app/ep2163_main.dart
+++ lib/ep2163_newsfeed_app/ep2163_main.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class NewsFeedApp extends StatelessWidget {
+  const NewsFeedApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
