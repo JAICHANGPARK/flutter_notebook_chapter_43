bbb341af76315ea029f5a107dde8b7ba417466b0
JAICHANGPARK <aristojeff@gmail.com>
2025-02-20 19:30:37 +0900

    Create news_feed_home_page.dart

---
+++ lib/ep2163_newsfeed_app/views/news_feed_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NewsFeedHomePage extends StatefulWidget {
+  const NewsFeedHomePage({super.key});
+
+  @override
+  State<NewsFeedHomePage> createState() => _NewsFeedHomePageState();
+}
+
+class _NewsFeedHomePageState extends State<NewsFeedHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
