a16c02e4787d7d0fe4e1eaaead8aa9d71f5498ff
JAICHANGPARK <aristojeff@gmail.com>
2025-02-25 18:42:38 +0900

    Update ep2168_main.dart

--- lib/ep2168_marketplace_app/ep2168_main.dart
+++ lib/ep2168_marketplace_app/ep2168_main.dart
@@ -1,2 +1,16 @@
 
 import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+
+class MarketPlaceApp extends StatelessWidget {
+  const MarketPlaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
