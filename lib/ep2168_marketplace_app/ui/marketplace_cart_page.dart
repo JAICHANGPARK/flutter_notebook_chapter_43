a4cf8f948f0538dc36c47c3e0ea5f4ecd058dadb
JAICHANGPARK <aristojeff@gmail.com>
2025-02-25 18:42:17 +0900

    Create marketplace_cart_page.dart

---
+++ lib/ep2168_marketplace_app/ui/marketplace_cart_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MarketplaceCartPage extends StatefulWidget {
+  const MarketplaceCartPage({super.key});
+
+  @override
+  State<MarketplaceCartPage> createState() => _MarketplaceCartPageState();
+}
+
+class _MarketplaceCartPageState extends State<MarketplaceCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
