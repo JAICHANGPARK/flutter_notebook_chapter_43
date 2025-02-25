be610a1d1879581d3f09a91ce5eed1bb8139d6e5
JAICHANGPARK <aristojeff@gmail.com>
2025-02-25 18:51:32 +0900

    Update marketplace_cart_page.dart

--- lib/ep2168_marketplace_app/ui/marketplace_cart_page.dart
+++ lib/ep2168_marketplace_app/ui/marketplace_cart_page.dart
@@ -86,6 +86,7 @@ class _MarketplaceCartPageState extends State<MarketplaceCartPage> {
                           backgroundColor: Colors.orangeAccent,
                           child: Icon(
                             Icons.chat_outlined,
+                            size: 14,
                           ),
                         ),
                         Container(
