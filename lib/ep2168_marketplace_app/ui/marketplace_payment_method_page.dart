ef5074e6e3dd329f6c8ba3cf77e7ada27d08a1ae
JAICHANGPARK <aristojeff@gmail.com>
2025-02-28 18:41:24 +0900

    Update marketplace_payment_method_page.dart

--- lib/ep2168_marketplace_app/ui/marketplace_payment_method_page.dart
+++ lib/ep2168_marketplace_app/ui/marketplace_payment_method_page.dart
@@ -13,154 +13,156 @@ class _MarketplacePaymentMethodPageState extends State<MarketplacePaymentMethodP
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            top: 24,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 24,
-                      backgroundColor: Colors.grey[200],
-                      foregroundColor: Colors.black,
-                      child: Icon(Icons.arrow_back),
-                    ),
-                    Expanded(
-                      child: Center(
-                        child: Text("Payment method")
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              top: 24,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 24,
+                        backgroundColor: Colors.grey[200],
+                        foregroundColor: Colors.black,
+                        child: Icon(Icons.arrow_back),
                       ),
-                    ),
-                    CircleAvatar(
-                      radius: 24,
-                      backgroundColor: Colors.grey[200],
-                      foregroundColor: Colors.black,
-                      child: Icon(Icons.menu),
-                    ),
-                  ],
-                ),
-                Gap(24),
-              ],
+                      Expanded(
+                        child: Center(
+                          child: Text("Payment method")
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 24,
+                        backgroundColor: Colors.grey[200],
+                        foregroundColor: Colors.black,
+                        child: Icon(Icons.menu),
+                      ),
+                    ],
+                  ),
+                  Gap(24),
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 72,
-              decoration: BoxDecoration(
-                // color: Colors.white,
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Colors.white,
-                    Colors.white.withValues(
-                      alpha: 0.4,
-                    )
-                  ],
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 72,
+                decoration: BoxDecoration(
+                  // color: Colors.white,
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.white,
+                      Colors.white.withValues(
+                        alpha: 0.4,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 24,
-            left: 0,
-            right: 0,
-            child: Row(
-              spacing: 12,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.orange[50],
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 10,
-                  ),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 12,
-                        backgroundColor: Colors.green,
-                        foregroundColor: Colors.white,
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            fontSize: 12,
+            Positioned(
+              bottom: 24,
+              left: 0,
+              right: 0,
+              child: Row(
+                spacing: 12,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.orange[50],
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 10,
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 12,
+                          backgroundColor: Colors.green,
+                          foregroundColor: Colors.white,
+                          child: Text(
+                            "1",
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
                           ),
                         ),
-                      ),
-                      Gap(12),
-                      Text(
-                        "\$75",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
+                        Gap(12),
+                        Text(
+                          "\$75",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  decoration: ShapeDecoration(
-                    shape: StadiumBorder(),
-                    color: Colors.orange[50],
-                  ),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 4,
-                    vertical: 4,
-                  ),
-                  child: Row(
-                    spacing: 8,
-                    children: [
-                      CircleAvatar(
-                        radius: 14,
-                        backgroundColor: Colors.white,
-                        child: Icon(
-                          Icons.chat_outlined,
-                          size: 14,
+                  Container(
+                    decoration: ShapeDecoration(
+                      shape: StadiumBorder(),
+                      color: Colors.orange[50],
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 4,
+                      vertical: 4,
+                    ),
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        CircleAvatar(
+                          radius: 14,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.chat_outlined,
+                            size: 14,
+                          ),
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                        decoration: ShapeDecoration(
-                          shape: StadiumBorder(),
-                          color: Colors.deepOrangeAccent,
-                        ),
-                        child: Row(
-                          spacing: 8,
-                          children: [
-                            Text(
-                              "Buy",
-                              style: TextStyle(
-                                color: Colors.white,
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                          decoration: ShapeDecoration(
+                            shape: StadiumBorder(),
+                            color: Colors.deepOrangeAccent,
+                          ),
+                          child: Row(
+                            spacing: 8,
+                            children: [
+                              Text(
+                                "Buy",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                            CircleAvatar(
-                              radius: 15,
-                              backgroundColor: Colors.white,
-                              foregroundColor: Colors.deepOrangeAccent,
-                              child: Icon(
-                                Icons.arrow_forward,
-                                size: 16,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                              CircleAvatar(
+                                radius: 15,
+                                backgroundColor: Colors.white,
+                                foregroundColor: Colors.deepOrangeAccent,
+                                child: Icon(
+                                  Icons.arrow_forward,
+                                  size: 16,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
