39ea7d56d4f490ba1dc62635e7958be3d38a9105
JAICHANGPARK <aristojeff@gmail.com>
2025-02-28 18:36:42 +0900

    Create marketplace_payment_method_page.dart

---
+++ lib/ep2168_marketplace_app/ui/marketplace_payment_method_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class MarketplacePaymentMethodPage extends StatefulWidget {
+  const MarketplacePaymentMethodPage({super.key});
+
+  @override
+  State<MarketplacePaymentMethodPage> createState() => _MarketplacePaymentMethodPageState();
+}
+
+class _MarketplacePaymentMethodPageState extends State<MarketplacePaymentMethodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
