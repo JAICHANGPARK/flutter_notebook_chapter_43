a3c6ededae60ea8ff63a5f1a9008643a9c7d148d
JAICHANGPARK <aristojeff@gmail.com>
2025-03-02 09:34:43 +0900

    Update habit_journal_main_screen.dart

--- lib/ep2173_habit_journal_app/ui/habit_journal_main_screen.dart
+++ lib/ep2173_habit_journal_app/ui/habit_journal_main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_43/ep2173_habit_journal_app/habit_app_theme.dart';
 
 class HabitJournalMainScreen extends StatefulWidget {
   const HabitJournalMainScreen({super.key});
@@ -18,11 +19,27 @@ class _HabitJournalMainScreenState extends State<HabitJournalMainScreen> {
             child: Placeholder(),
           ),
           Container(
-            height: 100,
+            height: 82,
+            padding: EdgeInsets.all(16),
             child: Row(
-              children: [],
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: HabitAppTheme.primary,
+                  ),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.home_filled,
+                      ),
+                      Text(
+                        "Home",
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-
           )
         ],
       ),
