7d09ddee4d13015be99f825f8c5e71319fecf851
JAICHANGPARK <aristojeff@gmail.com>
2025-03-02 09:23:34 +0900

    Update habit_journal_main_screen.dart

---
+++ lib/ep2173_habit_journal_app/ui/habit_journal_main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HabitJournalMainScreen extends StatefulWidget {
+  const HabitJournalMainScreen({super.key});
+
+  @override
+  State<HabitJournalMainScreen> createState() => _HabitJournalMainScreenState();
+}
+
+class _HabitJournalMainScreenState extends State<HabitJournalMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
