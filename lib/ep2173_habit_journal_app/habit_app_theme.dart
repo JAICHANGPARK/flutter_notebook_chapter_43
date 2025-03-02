95306d74030c45503643c521357c9c7abd72c83e
JAICHANGPARK <aristojeff@gmail.com>
2025-03-02 09:29:08 +0900

    Update habit_app_theme.dart

--- lib/ep2173_habit_journal_app/habit_app_theme.dart
+++ lib/ep2173_habit_journal_app/habit_app_theme.dart
@@ -2,6 +2,6 @@ import 'dart:ui';
 
 class HabitAppTheme {
   static Color background = Color.fromRGBO(233, 236, 246, 1);
-  static  Color primary = Color(0xff8C8AF3);
-  static  Color accent = Color(0xffD5FF00);
+  static Color primary = Color(0xff8C8AF3);
+  static Color accent = Color(0xffD5FF00);
 }
