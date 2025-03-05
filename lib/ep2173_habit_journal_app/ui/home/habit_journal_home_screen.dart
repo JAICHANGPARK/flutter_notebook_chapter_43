768dc1bab2ef8608c55e035b53aecae1c98f2c51
JAICHANGPARK <aristojeff@gmail.com>
2025-03-05 20:24:50 +0900

    Update habit_journal_home_screen.dart

--- lib/ep2173_habit_journal_app/ui/home/habit_journal_home_screen.dart
+++ lib/ep2173_habit_journal_app/ui/home/habit_journal_home_screen.dart
@@ -349,6 +349,7 @@ class _HabitJournalHomeScreenState extends State<HabitJournalHomeScreen> {
                 color: Color.fromRGBO(208, 204, 241, 1),
                 borderRadius: BorderRadius.circular(12),
               ),
+              padding: EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
