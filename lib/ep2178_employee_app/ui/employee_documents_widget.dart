3c1f471dfd6bcfe0ff27f5f3e22caab324c55da2
JAICHANGPARK <aristojeff@gmail.com>
2025-03-09 08:29:04 +0900

    Update employee_documents_widget.dart

---
+++ lib/ep2178_employee_app/ui/employee_documents_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmployeeDocumentsWidget extends StatefulWidget {
+  const EmployeeDocumentsWidget({super.key});
+
+  @override
+  State<EmployeeDocumentsWidget> createState() => _EmployeeDocumentsWidgetState();
+}
+
+class _EmployeeDocumentsWidgetState extends State<EmployeeDocumentsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
