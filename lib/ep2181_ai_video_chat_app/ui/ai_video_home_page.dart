f89e2fbb957be152b609ae2acd3bbf5755cfa0f2
JAICHANGPARK <aristojeff@gmail.com>
2025-03-10 18:46:28 +0900

    Create ai_video_home_page.dart

---
+++ lib/ep2181_ai_video_chat_app/ui/ai_video_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiVideoHomePage extends StatefulWidget {
+  const AiVideoHomePage({super.key});
+
+  @override
+  State<AiVideoHomePage> createState() => _AiVideoHomePageState();
+}
+
+class _AiVideoHomePageState extends State<AiVideoHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
