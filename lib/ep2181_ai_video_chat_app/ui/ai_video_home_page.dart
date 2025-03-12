b53e4eb1cc8497624135389e398f30233268d170
JAICHANGPARK <aristojeff@gmail.com>
2025-03-12 19:40:54 +0900

    Update ai_video_home_page.dart

--- lib/ep2181_ai_video_chat_app/ui/ai_video_home_page.dart
+++ lib/ep2181_ai_video_chat_app/ui/ai_video_home_page.dart
@@ -140,17 +140,21 @@ class _AiVideoHomePageState extends State<AiVideoHomePage> {
                   Container(
                     height: 180,
                     child: CarouselView.weighted(
-                      flexWeights: [1, 8,1],
+                      flexWeights: [1, 8, 1],
                       children: List.generate(10, (idx) {
-                        return Container(
-                          
-                        );
+                        return Container();
                       }),
                     ),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text("Active"), TextButton(onPressed: () {}, child: Text("See all"))],
+                    children: [
+                      Text("Active"),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text("See all"),
+                      ),
+                    ],
                   ),
                   Container(
                     height: 100,
