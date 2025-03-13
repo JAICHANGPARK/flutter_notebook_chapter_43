3df8e123a8d4c1ffd21d862ec13f804b4e92ea0d
JAICHANGPARK <aristojeff@gmail.com>
2025-03-13 20:42:07 +0900

    Update ai_video_detail_page.dart

--- lib/ep2181_ai_video_chat_app/ui/ai_video_detail_page.dart
+++ lib/ep2181_ai_video_chat_app/ui/ai_video_detail_page.dart
@@ -32,7 +32,9 @@ class _AiVideoDetailPageState extends State<AiVideoDetailPage> {
                       shape: StadiumBorder(),
                       color: Colors.white.withValues(alpha: .3),
                     ),
+                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
                     child: Row(
+                      spacing: 4,
                       children: [
                         CircleAvatar(
                           radius: 3,
