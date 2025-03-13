83d8391651df4bcd09c34b01def084deef1ba90f
JAICHANGPARK <aristojeff@gmail.com>
2025-03-13 20:35:10 +0900

    Update ai_video_detail_page.dart

---
+++ lib/ep2181_ai_video_chat_app/ui/ai_video_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiVideoDetailPage extends StatefulWidget {
+  const AiVideoDetailPage({super.key});
+
+  @override
+  State<AiVideoDetailPage> createState() => _AiVideoDetailPageState();
+}
+
+class _AiVideoDetailPageState extends State<AiVideoDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
