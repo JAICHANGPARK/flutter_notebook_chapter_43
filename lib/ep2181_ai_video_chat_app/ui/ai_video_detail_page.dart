21d839d650a00cd099276e8f88e55c3d622ef524
JAICHANGPARK <aristojeff@gmail.com>
2025-03-14 18:08:02 +0900

    Update ai_video_detail_page.dart

--- lib/ep2181_ai_video_chat_app/ui/ai_video_detail_page.dart
+++ lib/ep2181_ai_video_chat_app/ui/ai_video_detail_page.dart
@@ -167,6 +167,9 @@ class _AiVideoDetailPageState extends State<AiVideoDetailPage> {
                                       child: Center(
                                         child: Text(
                                           "Dream",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
                                         ),
                                       ),
                                     ),
@@ -181,6 +184,33 @@ class _AiVideoDetailPageState extends State<AiVideoDetailPage> {
                                 color: Colors.white,
                                 borderRadius: BorderRadius.circular(16),
                               ),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    bottom: 4,
+                                    left: 4,
+                                    right: 4,
+                                    child: Container(
+                                      decoration: ShapeDecoration(
+                                        shape: StadiumBorder(),
+                                        color: Colors.grey,
+                                      ),
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 8,
+                                        vertical: 5,
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "Dream",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                           Expanded(
@@ -188,6 +218,33 @@ class _AiVideoDetailPageState extends State<AiVideoDetailPage> {
                               decoration: BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    bottom: 4,
+                                    left: 4,
+                                    right: 4,
+                                    child: Container(
+                                      decoration: ShapeDecoration(
+                                        shape: StadiumBorder(),
+                                        color: Colors.grey,
+                                      ),
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 8,
+                                        vertical: 5,
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "Dream",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
