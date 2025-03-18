938485949de886a57f1d83117d8b4394f0cf11de
JAICHANGPARK <aristojeff@gmail.com>
2025-03-18 18:48:42 +0900

    Update event_purchase_page.dart

---
+++ lib/ep2186_event_app/ui/event_purchase_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventPurchasePage extends StatefulWidget {
+  const EventPurchasePage({super.key});
+
+  @override
+  State<EventPurchasePage> createState() => _EventPurchasePageState();
+}
+
+class _EventPurchasePageState extends State<EventPurchasePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
