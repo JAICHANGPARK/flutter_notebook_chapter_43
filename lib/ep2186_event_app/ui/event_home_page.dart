1dca59f8bb880d7e94a82e9b8078fb9254f5dd5f
JAICHANGPARK <aristojeff@gmail.com>
2025-03-18 18:50:03 +0900

    Update event_home_page.dart

--- lib/ep2186_event_app/ui/event_home_page.dart
+++ lib/ep2186_event_app/ui/event_home_page.dart
@@ -123,109 +123,111 @@ class _EventHomePageState extends State<EventHomePage> {
                         child: Column(
                           spacing: 12,
                           children: [
-                            Container(
-                              // height: 280,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                color: Color.fromRGBO(35, 35, 35, 1),
-                              ),
-                              child: IntrinsicHeight(
-                                child: Row(
-                                  // spacing: 12,
-                                  children: [
-                                    Container(
-                                      width: 120,
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(16),
-                                          bottomLeft: Radius.circular(16),
+                            GestureDetector(
+                              child: Container(
+                                // height: 280,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  color: Color.fromRGBO(35, 35, 35, 1),
+                                ),
+                                child: IntrinsicHeight(
+                                  child: Row(
+                                    // spacing: 12,
+                                    children: [
+                                      Container(
+                                        width: 120,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(16),
+                                            bottomLeft: Radius.circular(16),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Expanded(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(16.0),
-                                        child: Column(
-                                          spacing: 12,
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Row(
-                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                              children: [
-                                                Expanded(
-                                                  child: Text(
-                                                    "A New look at Watteau",
-                                                    style: TextStyle(
-                                                      fontSize: 20,
-                                                      color: Colors.white,
-                                                      fontWeight: FontWeight.bold,
+                                      Expanded(
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Column(
+                                            spacing: 12,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Row(
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                children: [
+                                                  Expanded(
+                                                    child: Text(
+                                                      "A New look at Watteau",
+                                                      style: TextStyle(
+                                                        fontSize: 20,
+                                                        color: Colors.white,
+                                                        fontWeight: FontWeight.bold,
+                                                      ),
                                                     ),
                                                   ),
-                                                ),
-                                                IconButton(
-                                                  onPressed: () {},
-                                                  icon: Icon(
-                                                    Icons.favorite,
-                                                  ),
-                                                  color: Colors.orange,
-                                                )
-                                              ],
-                                            ),
-                                            Text(
-                                              "An actor with no lines:\nPierrot, knows as Gilles",
-                                              style: TextStyle(
-                                                // fontSize: 12,
-                                                color: Colors.white,
+                                                  IconButton(
+                                                    onPressed: () {},
+                                                    icon: Icon(
+                                                      Icons.favorite,
+                                                    ),
+                                                    color: Colors.orange,
+                                                  )
+                                                ],
                                               ),
-                                            ),
-                                            Row(
-                                              spacing: 8,
-                                              children: [
-                                                Icon(
-                                                  Icons.location_on_outlined,
-                                                  color: Colors.grey,
-                                                  size: 16,
+                                              Text(
+                                                "An actor with no lines:\nPierrot, knows as Gilles",
+                                                style: TextStyle(
+                                                  // fontSize: 12,
+                                                  color: Colors.white,
                                                 ),
-                                                Text(
-                                                  "Dream Museum",
-                                                  style: TextStyle(
-                                                    // fontSize: 12,
-                                                    color: Colors.grey,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                            Row(
-                                              spacing: 8,
-                                              children: [
-                                                Icon(
-                                                  Icons.calendar_month,
-                                                  color: Colors.grey,
-                                                  size: 16,
-                                                ),
-                                                Text(
-                                                  "16 Oct - 3 Feb 2025",
-                                                  style: TextStyle(
-                                                    // fontSize: 12,
-                                                    color: Colors.grey,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                            Text(
-                                              "\$14.00",
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 16,
                                               ),
-                                            )
-                                          ],
+                                              Row(
+                                                spacing: 8,
+                                                children: [
+                                                  Icon(
+                                                    Icons.location_on_outlined,
+                                                    color: Colors.grey,
+                                                    size: 16,
+                                                  ),
+                                                  Text(
+                                                    "Dream Museum",
+                                                    style: TextStyle(
+                                                      // fontSize: 12,
+                                                      color: Colors.grey,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              Row(
+                                                spacing: 8,
+                                                children: [
+                                                  Icon(
+                                                    Icons.calendar_month,
+                                                    color: Colors.grey,
+                                                    size: 16,
+                                                  ),
+                                                  Text(
+                                                    "16 Oct - 3 Feb 2025",
+                                                    style: TextStyle(
+                                                      // fontSize: 12,
+                                                      color: Colors.grey,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              Text(
+                                                "\$14.00",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 16,
+                                                ),
+                                              )
+                                            ],
+                                          ),
                                         ),
-                                      ),
-                                    )
-                                  ],
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
