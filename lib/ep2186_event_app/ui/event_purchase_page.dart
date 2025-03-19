e25477677e60437ed4d6d603f754ea10a347c082
JAICHANGPARK <aristojeff@gmail.com>
2025-03-19 20:56:51 +0900

    Update event_purchase_page.dart

--- lib/ep2186_event_app/ui/event_purchase_page.dart
+++ lib/ep2186_event_app/ui/event_purchase_page.dart
@@ -266,7 +266,10 @@ class _EventPurchasePageState extends State<EventPurchasePage> {
                                 color: Colors.white,
                               ),
                             ),
-                            Text("\$ 12.00"),
+                            Text("\$ 12.00",
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),),
                           ],
                         ),
                       ),
