eef559be883dc092eef3503dd9571523cc15e20d
JAICHANGPARK <aristojeff@gmail.com>
2025-03-22 09:34:22 +0900

    Update food_order_main_page.dart

--- lib/ep2393_food_order_app/ui/food_order_main_page.dart
+++ lib/ep2393_food_order_app/ui/food_order_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FoodOrderMainPage extends StatefulWidget {
   const FoodOrderMainPage({super.key});
 
@@ -17,6 +16,9 @@ class _FoodOrderMainPageState extends State<FoodOrderMainPage> {
         children: [
           Container(
             height: 160,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(15, 17, 5, 1),
+            ),
           )
         ],
       ),
