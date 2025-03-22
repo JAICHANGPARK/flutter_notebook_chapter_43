ac3bc48d1e6c8db43e04c9f5b6ac81597ba020d7
JAICHANGPARK <aristojeff@gmail.com>
2025-03-23 08:48:19 +0900

    Update food_order_main_page.dart

--- lib/ep2393_food_order_app/ui/food_order_main_page.dart
+++ lib/ep2393_food_order_app/ui/food_order_main_page.dart
@@ -99,6 +99,7 @@ class _FoodOrderMainPageState extends State<FoodOrderMainPage> {
                     CircleAvatar(
                       radius: 52 / 2,
                       backgroundColor: Color.fromRGBO(28, 33, 11, 1),
+                      foregroundColor: Colors.white,
                       child: Icon(Icons.access_time),
                     ),
                   ],
