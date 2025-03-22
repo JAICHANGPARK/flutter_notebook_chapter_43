7e3aa733617bc6a45d0d04eeee8289b3026fd582
JAICHANGPARK <aristojeff@gmail.com>
2025-03-22 09:31:11 +0900

    Update food_order_main_page.dart

---
+++ lib/ep2393_food_order_app/ui/food_order_main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class FoodOrderMainPage extends StatefulWidget {
+  const FoodOrderMainPage({super.key});
+
+  @override
+  State<FoodOrderMainPage> createState() => _FoodOrderMainPageState();
+}
+
+class _FoodOrderMainPageState extends State<FoodOrderMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
