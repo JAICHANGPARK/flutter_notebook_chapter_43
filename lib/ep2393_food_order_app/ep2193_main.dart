583c72d7baaa66a2e14682dfcde800c7891449a1
JAICHANGPARK <aristojeff@gmail.com>
2025-03-22 09:29:24 +0900

    Update ep2193_main.dart

---
+++ lib/ep2393_food_order_app/ep2193_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FoodORderApp extends StatelessWidget {
+  const FoodORderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
