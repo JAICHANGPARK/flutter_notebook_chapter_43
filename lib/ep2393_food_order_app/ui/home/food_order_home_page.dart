e339a4d234208cb3daf6702005e81e3497ad331d
JAICHANGPARK <aristojeff@gmail.com>
2025-03-24 19:41:01 +0900

    Update food_order_home_page.dart

--- lib/ep2393_food_order_app/ui/home/food_order_home_page.dart
+++ lib/ep2393_food_order_app/ui/home/food_order_home_page.dart
@@ -10,6 +10,6 @@ class FoodOrderHomePage extends StatefulWidget {
 class _FoodOrderHomePageState extends State<FoodOrderHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView();
   }
 }
