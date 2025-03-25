fe8340e866613cc308a3d221cd41379e799584ac
JAICHANGPARK <aristojeff@gmail.com>
2025-03-25 18:44:53 +0900

    Update food_order_home_page.dart

--- lib/ep2393_food_order_app/ui/home/food_order_home_page.dart
+++ lib/ep2393_food_order_app/ui/home/food_order_home_page.dart
@@ -150,15 +150,16 @@ class _FoodOrderHomePageState extends State<FoodOrderHomePage> {
                   children: [
                     Expanded(
                       child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                        child: Stack(
-                          children: [
-
-                          ],
-                        )
-                      ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  "https://cdn.pixabay.com/photo/2017/06/17/15/06/pan-pizza-2412474_1280.jpg",
+                                ),
+                              )),
+                          child: Stack(
+                            children: [],
+                          )),
                     ),
                     Row(
                       spacing: 6,
