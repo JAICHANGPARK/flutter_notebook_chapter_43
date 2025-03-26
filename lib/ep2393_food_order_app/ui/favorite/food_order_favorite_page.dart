e17a5da1ced96019ace3625044ac717732505e6e
JAICHANGPARK <aristojeff@gmail.com>
2025-03-26 20:29:00 +0900

    Update food_order_favorite_page.dart

--- lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
+++ lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
@@ -26,7 +26,7 @@ class _FoodOrderFavoritePageState extends State<FoodOrderFavoritePage> {
                 crossAxisCount: 2,
                 crossAxisSpacing: 16,
                 mainAxisSpacing: 16,
-                childAspectRatio: .8,
+                childAspectRatio: .75,
               ),
               itemBuilder: (context, index) {
                 return Column(
