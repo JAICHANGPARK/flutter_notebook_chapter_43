146672863eb9d00b5f24a6183a1bef8a3256e8d9
JAICHANGPARK <aristojeff@gmail.com>
2025-03-26 20:22:02 +0900

    Update food_order_favorite_page.dart

--- lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
+++ lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class FoodOrderFavoritePage extends StatefulWidget {
@@ -17,9 +19,98 @@ class _FoodOrderFavoritePageState extends State<FoodOrderFavoritePage> {
           child: Placeholder(),
         ),
         Expanded(
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemBuilder: (context, index) {},
+          child:GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            childAspectRatio: .9,
+            padding: EdgeInsets.zero,
+            physics: NeverScrollableScrollPhysics(),
+            children: List.generate(10, (idx) {
+              return Column(
+                spacing: 6,
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://cdn.pixabay.com/photo/2017/06/17/15/06/pan-pizza-2412474_1280.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                              left: 0,
+                              right: 0,
+                              bottom: 0,
+                              child: ClipRect(
+                                child: BackdropFilter(
+                                  filter: ImageFilter.blur(sigmaX: 4, sigmaY: 6),
+                                  child: Container(
+                                    padding: EdgeInsets.all(12),
+                                    child: Text.rich(TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: "\$75.00/",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: "\$90.00",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 10,
+                                            decoration: TextDecoration.lineThrough,
+                                          ),
+                                        ),
+                                      ],
+                                    )),
+                                  ),
+                                ),
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                  Row(
+                    spacing: 6,
+                    children: [
+                      CircleAvatar(
+                        radius: 16,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "OperaHouse Pizza",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Text(
+                              "Unknown Places",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              );
+            }),
           ),
         )
       ],
