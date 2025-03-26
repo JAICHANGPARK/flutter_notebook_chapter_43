b1fb58ac0b722a3469939bcecb4da363059f280a
JAICHANGPARK <aristojeff@gmail.com>
2025-03-26 20:21:42 +0900

    Update food_order_favorite_page.dart

--- lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
+++ lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
@@ -10,12 +10,19 @@ class FoodOrderFavoritePage extends StatefulWidget {
 class _FoodOrderFavoritePageState extends State<FoodOrderFavoritePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 62, child: Placeholder()),
-        ],
-      ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 62,
+          child: Placeholder(),
+        ),
+        Expanded(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (context, index) {},
+          ),
+        )
+      ],
     );
   }
 }
