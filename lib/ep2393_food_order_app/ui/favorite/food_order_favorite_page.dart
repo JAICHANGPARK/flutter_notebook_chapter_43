9498254d5cecbf432bac83b7acaa64e214ab28bd
JAICHANGPARK <aristojeff@gmail.com>
2025-03-26 20:20:15 +0900

    Update food_order_favorite_page.dart

--- lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
+++ lib/ep2393_food_order_app/ui/favorite/food_order_favorite_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FoodOrderFavoritePage extends StatefulWidget {
   const FoodOrderFavoritePage({super.key});
 
@@ -14,7 +13,7 @@ class _FoodOrderFavoritePageState extends State<FoodOrderFavoritePage> {
     return Scaffold(
       body: Column(
         children: [
-          
+          SizedBox(height: 62, child: Placeholder()),
         ],
       ),
     );
