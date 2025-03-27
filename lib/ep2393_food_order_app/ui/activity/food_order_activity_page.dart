15537800843a7fe44ab155d50b69746053ca38a0
JAICHANGPARK <aristojeff@gmail.com>
2025-03-27 22:49:11 +0900

    Update food_order_activity_page.dart

--- lib/ep2393_food_order_app/ui/activity/food_order_activity_page.dart
+++ lib/ep2393_food_order_app/ui/activity/food_order_activity_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class FoodOrderActivityPage extends StatefulWidget {
+  const FoodOrderActivityPage({super.key});
+
+  @override
+  State<FoodOrderActivityPage> createState() => _FoodOrderActivityPageState();
+}
+
+class _FoodOrderActivityPageState extends State<FoodOrderActivityPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
