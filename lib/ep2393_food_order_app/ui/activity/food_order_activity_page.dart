74f3093998cc34aeb6a5eb65063fd649ef0e5083
JAICHANGPARK <aristojeff@gmail.com>
2025-03-28 20:01:52 +0900

    Update food_order_activity_page.dart

--- lib/ep2393_food_order_app/ui/activity/food_order_activity_page.dart
+++ lib/ep2393_food_order_app/ui/activity/food_order_activity_page.dart
@@ -39,183 +39,97 @@ class _FoodOrderActivityPageState extends State<FoodOrderActivityPage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               spacing: 12,
-              children: [
-                Container(
-                  height: 440,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withValues(alpha: .1),
-                  ),
-                  padding: EdgeInsets.all(12),
-                  child: Column(
-                    spacing: 12,
-                    children: [
-                      Row(
-                        spacing: 12,
-                        children: [
-                          CircleAvatar(),
-                          Text(
-                            "Dreamwalker",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
+              children: List.generate(
+                  10,
+                  (index) => Container(
+                        height: 440,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withValues(alpha: .1),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: Column(
+                          spacing: 12,
+                          children: [
+                            Row(
+                              spacing: 12,
+                              children: [
+                                CircleAvatar(),
+                                Text(
+                                  "Dreamwalker",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Spacer(),
+                                CircleAvatar(
+                                  radius: 16,
+                                  backgroundColor: Colors.white.withValues(alpha: .2),
+                                  foregroundColor: Colors.grey,
+                                  child: Icon(Icons.more_horiz),
+                                )
+                              ],
                             ),
-                          ),
-                          Spacer(),
-                          CircleAvatar(
-                            radius: 16,
-                            backgroundColor: Colors.white.withValues(alpha: .2),
-                            foregroundColor: Colors.grey,
-                            child: Icon(Icons.more_horiz),
-                          )
-                        ],
-                      ),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white.withValues(alpha: .2),
-                          ),
-                          child: Column(
-                            spacing: 12,
-                            children: [
-                              Container(
-                                height: 180,
-                                child: Placeholder(),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(12.0),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white.withValues(alpha: .2),
+                                ),
                                 child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  spacing: 12,
                                   children: [
-                                    Text("Preparing pasta in pan"),
-                                    Text(
-                                      "This baked salmon with a crunchy topping takes just 20 minutes",
+                                    Container(
+                                      height: 180,
+                                      child: Placeholder(),
                                     ),
-                                    Row(
-                                      children: [],
-                                    ),
-                                    Divider(
-                                      color: Colors.white.withValues(alpha: .2),
-                                      height: 32,
-                                      // indent: 12,
-                                      // endIndent: 12,
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "Add Comment",
-                                          style: TextStyle(
-                                            color: Colors.grey,
+                                    Padding(
+                                      padding: const EdgeInsets.all(12.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text("Preparing pasta in pan"),
+                                          Text(
+                                            "This baked salmon with a crunchy topping takes just 20 minutes",
                                           ),
-                                        ),
-                                        Spacer(),
-                                        CircleAvatar(
-                                          radius: 16,
-                                        ),
-                                        CircleAvatar(
-                                          radius: 16,
-                                        ),
-                                        CircleAvatar(
-                                          radius: 16,
-                                        ),
-                                      ],
+                                          Row(
+                                            children: [],
+                                          ),
+                                          Divider(
+                                            color: Colors.white.withValues(alpha: .2),
+                                            height: 32,
+                                            // indent: 12,
+                                            // endIndent: 12,
+                                          ),
+                                          Row(
+                                            children: [
+                                              Text(
+                                                "Add Comment",
+                                                style: TextStyle(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                              Spacer(),
+                                              CircleAvatar(
+                                                radius: 16,
+                                              ),
+                                              CircleAvatar(
+                                                radius: 16,
+                                              ),
+                                              CircleAvatar(
+                                                radius: 16,
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
                                     )
                                   ],
                                 ),
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 440,
-                  decoration: BoxDecoration(color: Colors.white.withValues(alpha: .1)),
-                  padding: EdgeInsets.all(12),
-                  child: Column(
-                    spacing: 12,
-                    children: [
-                      Row(
-                        spacing: 12,
-                        children: [
-                          CircleAvatar(),
-                          Text(
-                            "Dreamwalker",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Spacer(),
-                          CircleAvatar(
-                            backgroundColor: Colors.white.withValues(alpha: .2),
-                            radius: 16,
-                            child: Icon(Icons.more_horiz),
-                          )
-                        ],
-                      ),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white.withValues(alpha: .2),
-                          ),
-                          child: Column(
-                            spacing: 12,
-                            children: [
-                              Container(
-                                height: 180,
-                                child: Placeholder(),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.all(12.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Preparing pasta in pan"),
-                                    Text(
-                                      "This baked salmon with a crunchy topping takes just 20 minutes",
-                                    ),
-                                    Row(
-                                      children: [],
-                                    ),
-                                    Divider(
-                                      color: Colors.white.withValues(alpha: .2),
-                                      height: 32,
-                                      // indent: 12,
-                                      // endIndent: 12,
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "Add Comment",
-                                          style: TextStyle(
-                                            color: Colors.grey,
-                                          ),
-                                        ),
-                                        Spacer(),
-                                        CircleAvatar(
-                                          radius: 16,
-                                        ),
-                                        CircleAvatar(
-                                          radius: 16,
-                                        ),
-                                        CircleAvatar(
-                                          radius: 16,
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                      )),
             ),
           ))
         ],
