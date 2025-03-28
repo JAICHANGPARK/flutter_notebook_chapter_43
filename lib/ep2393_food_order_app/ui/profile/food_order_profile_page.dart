f004bcea34e998d61c1b382a59d99b12b772f96e
JAICHANGPARK <aristojeff@gmail.com>
2025-03-29 08:51:20 +0900

    Update food_order_profile_page.dart

--- lib/ep2393_food_order_app/ui/profile/food_order_profile_page.dart
+++ lib/ep2393_food_order_app/ui/profile/food_order_profile_page.dart
@@ -11,6 +11,13 @@ class FoodOrderProfilePage extends StatefulWidget {
 class _FoodOrderProfilePageState extends State<FoodOrderProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    )
+    ;
   }
 }
